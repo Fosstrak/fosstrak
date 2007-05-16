0ae8bedf46ce289216829531c5f6fc263d65e955
mlampe <mlampe@b63d296a-7662-4df4-3098-00f9c2c9103a>
2007-05-16 19:55:06 +0000

    changed version to reader-0.4.0 and added CRC16.java

--- reader/trunk/reader-hal/src/main/java/org/fosstrak/reader/hal/HardwareAbstraction.java
+++ reader/trunk/reader-hal/src/main/java/org/fosstrak/reader/hal/HardwareAbstraction.java
@@ -18,12 +18,6 @@
  * Boston, MA  02110-1301  USA
  */
 
-/*
- * Created on 18.01.2004
- *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
 package org.accada.reader.hal;
 
 
