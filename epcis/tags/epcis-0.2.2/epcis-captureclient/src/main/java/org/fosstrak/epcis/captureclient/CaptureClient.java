f7ebe9d9dba3e034c4ebf871fbbc3d92976f59b7
roduner <roduner@b63d296a-7662-4df4-3098-00f9c2c9103a>
2007-08-31 13:56:20 +0000

    [maven-scm] copy for tag epcis-0.2.2

---
+++ epcis/tags/epcis-0.2.2/epcis-captureclient/src/main/java/org/fosstrak/epcis/captureclient/CaptureClient.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.accada.epcis.captureclient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+
+/**
+ * This client provides access to the repository's Capture Operations Module
+ * through the capture interface. EPCISEvents will be sent to the module using
+ * HTTP POST requests.
+ * 
+ * @author Marco Steybe
+ */
+public class CaptureClient implements CaptureInterface {
+
+    private static final String PROPERTY_FILE = "/captureclient.properties";
+
+    private static final String PROPERTY_CAPTURE_URL = "default.url";
+
+    /**
+     * The URL String at which the Capture Operations Module listens.
+     */
+    private String captureUrl;
+
+    private Properties props = new Properties();
+
+    /**
+     * Constructs a new CaptureClient which connects to the Capture Operations
+     * Module listening at the default URL.
+     */
+    public CaptureClient() {
+        this(null);
+    }
+
+    /**
+     * Constructs a new CaptureClient which connects to the Capture Operations
+     * Module listening at the given URL.
+     * 
+     * @param url
+     *            The URL at which the capture service listens.
+     */
+    public CaptureClient(final String url) {
+        // load properties
+        InputStream is = this.getClass().getResourceAsStream(PROPERTY_FILE);
+        if (is == null) {
+            throw new RuntimeException("Unable to load properties from file " + PROPERTY_FILE);
+        }
+        try {
+            props.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to load properties from file " + PROPERTY_FILE);
+        }
+
+        // set the URL
+        if (url != null) {
+            captureUrl = url;
+        } else {
+            captureUrl = props.getProperty(PROPERTY_CAPTURE_URL);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.accada.epcis.captureclient.CaptureInterface#capture(java.io.InputStream)
+     */
+    public int capture(final InputStream xmlStream) throws IOException {
+        return doPost(xmlStream, "text/xml");
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.accada.epcis.captureclient.CaptureInterface#capture(java.lang.String)
+     */
+    public int capture(final String eventXml) throws IOException {
+        return doPost(eventXml, "text/xml");
+    }
+
+    /**
+     * Invokes the dbReset operation in the Capture Module deleting all event
+     * data in the EPCIS database. This operation is only allowed if the
+     * corresponding property is set in the repository's configuration.
+     * 
+     * @return The response from the capture module.
+     * @throws IOException
+     *             If a communication error occured.
+     */
+    public int dbReset() throws IOException {
+        String formParam = "dbReset=true";
+        return doPost(formParam, "application/x-www-form-urlencoded");
+
+    }
+
+    /**
+     * Opens a connection to the capture module.
+     * 
+     * @param contentType
+     * @return
+     * @throws IOException
+     */
+    private HttpURLConnection openPostConnection(String contentType) throws IOException {
+        // the url where the capture interface listens
+        URL serviceUrl = new URL(captureUrl);
+        HttpURLConnection connection = (HttpURLConnection) serviceUrl.openConnection();
+        connection.setRequestProperty("content-type", contentType);
+        connection.setRequestMethod("POST");
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        return connection;
+    }
+
+    /**
+     * Send data to the repository's capture operation using HTTP POST. The data
+     * will be sent using the given content-type.
+     * 
+     * @param data
+     *            The data to send.
+     * @return The HTTP response message
+     * @throws IOException
+     *             If an error on the HTTP layer occured.
+     */
+    private int doPost(final String data, final String contentType) throws IOException {
+        HttpURLConnection connection = openPostConnection(contentType);
+        // write the data
+        OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+        wr.write(data);
+        wr.flush();
+        wr.close();
+
+        return connection.getResponseCode();
+    }
+
+    /**
+     * Send data to the repository's capture operation using HTTP POST. The data
+     * will be sent using the given content-type.
+     * 
+     * @param data
+     *            The data to send.
+     * @return The HTTP response message
+     * @throws IOException
+     *             If an error on the HTTP layer occured.
+     */
+    private int doPost(final InputStream data, final String contentType) throws IOException {
+        HttpURLConnection connection = openPostConnection(contentType);
+        // read from input and write to output
+        OutputStream os = connection.getOutputStream();
+        int b;
+        while ((b = data.read()) != -1) {
+            os.write(b);
+        }
+        os.flush();
+        os.close();
+
+        return connection.getResponseCode();
+    }
+
+    /**
+     * @return The URL String at which the Capture Operations Module listens.
+     */
+    public String getCaptureUrl() {
+        return captureUrl;
+    }
+
+    /**
+     * @param url
+     *            The new URL String to which this client should connect.
+     */
+    public void setCaptureUrl(String url) {
+        this.captureUrl = url;
+    }
+}
---
+++ epcis/tags/epcis-0.2.2/epcis-queryclient/src/main/java/org/fosstrak/epcis/queryclient/QueryControlInterface.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.accada.epcis.queryclient;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+import javax.xml.rpc.ServiceException;
+
+import org.accada.epcis.soapapi.QueryParam;
+import org.accada.epcis.soapapi.QueryResults;
+import org.accada.epcis.soapapi.SubscriptionControls;
+import org.apache.axis.types.URI;
+
+/**
+ * @author Marco Steybe
+ */
+public interface QueryControlInterface {
+
+    /**
+     * Performs a poll operation at the repository's Query Controls Module, i.e.
+     * runs a query with the given name and parameters.
+     * 
+     * @param queryName
+     *            The name of the query to be executed.
+     * @param params
+     *            The parameters of the query to be executed.
+     * @return The QueryResults.
+     * @throws ServiceException
+     *             If an exception whithin the repository's Query Controls
+     *             Module occured.
+     * @throws RemoteException
+     *             If an exception communicating with the repository's Query
+     *             Controls Module occured.
+     */
+    QueryResults poll(final String queryName, final QueryParam[] params)
+            throws ServiceException, RemoteException;
+
+    /**
+     * Performs a subscribe operation at the repository's Query Controls Module,
+     * i.e. subscribes a query for later execution.
+     * 
+     * @param queryName
+     *            The name of the query to be executed.
+     * @param params
+     *            The parameters of the query to be executed.
+     * @param dest
+     *            The destination address where QueryResults will be sent once
+     *            the subscribed query gets executed.
+     * @param controls
+     *            The SubscriptionControls.
+     * @param subscriptionId
+     *            The ID of the subscription.
+     * @throws ServiceException
+     *             If an exception whithin the repository's Query Controls
+     *             Module occured.
+     * @throws RemoteException
+     *             If an exception communicating with the repository's Query
+     *             Controls Module occured.
+     */
+    void subscribe(final String queryName, final QueryParam[] params,
+            final URI dest, final SubscriptionControls controls,
+            final String subscriptionId) throws ServiceException,
+            RemoteException;
+
+    /**
+     * Perform an unsubscribe operation at the repository's Query Controls
+     * Module, i.e. unsubscribes a previously subscribed query.
+     * 
+     * @param subscriptionId
+     *            The ID of the query to be unsubscribed.
+     * @throws ServiceException
+     *             If an exception whithin the repository's Query Controls
+     *             Module occured.
+     * @throws RemoteException
+     *             If an exception communicating with the repository's Query
+     *             Controls Module occured.
+     */
+    void unsubscribe(final String subscriptionId) throws ServiceException,
+            RemoteException;
+
+    /**
+     * Retrieves the names of queries that can be coped with.
+     * 
+     * @return A List of query names.
+     * @throws ServiceException
+     *             If an exception whithin the repository's Query Controls
+     *             Module occured.
+     * @throws RemoteException
+     *             If an exception communicating with the repository's Query
+     *             Controls Module occured.
+     */
+    List<String> getQueryNames() throws ServiceException, RemoteException;
+
+    /**
+     * Retrieves the ID of a subscribed query.
+     * 
+     * @param queryName
+     *            The name of the query.
+     * @return A List of IDs.
+     * @throws ServiceException
+     *             If an exception whithin the repository's Query Controls
+     *             Module occured.
+     * @throws RemoteException
+     *             If an exception communicating with the repository's Query
+     *             Controls Module occured.
+     */
+    List<String> getSubscriptionIds(final String queryName)
+            throws ServiceException, RemoteException;
+
+    /**
+     * Retrieves the standard version implemented by this implementation.
+     * 
+     * @return The implemented standard version.
+     * @throws ServiceException
+     *             If an exception whithin the repository's Query Controls
+     *             Module occured.
+     * @throws RemoteException
+     *             If an exception communicating with the repository's Query
+     *             Controls Module occured.
+     */
+    String getStandardVersion() throws ServiceException, RemoteException;
+
+    /**
+     * Retrieves the vendor version.
+     * 
+     * @return The vendor version.
+     * @throws ServiceException
+     *             If an exception whithin the repository's Query Controls
+     *             Module occured.
+     * @throws RemoteException
+     *             If an exception communicating with the repository's Query
+     *             Controls Module occured.
+     */
+    String getVendorVersion() throws ServiceException, RemoteException;
+}
