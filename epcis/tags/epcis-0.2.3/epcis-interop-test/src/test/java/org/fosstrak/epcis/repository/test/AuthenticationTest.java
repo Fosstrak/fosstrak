76b35a31fba785df3cafe35d58340235bcfb3f0a
roduner <roduner@b63d296a-7662-4df4-3098-00f9c2c9103a>
2007-12-14 13:57:57 +0000

    [maven-scm] copy for tag epcis-0.2.3

---
+++ epcis/tags/epcis-0.2.3/epcis-interop-test/src/test/java/org/fosstrak/epcis/repository/test/AuthenticationTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.accada.epcis.repository.test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.rpc.ServiceException;
+
+import junit.framework.TestCase;
+
+import org.accada.epcis.queryclient.QueryControlClient;
+import org.accada.epcis.soapapi.QueryResults;
+import org.accada.epcis.utils.QueryResultsParser;
+
+/**
+ * @author Marco Steybe
+ */
+public class AuthenticationTest extends TestCase {
+
+    private static final String REQ_PATH = "src/test/resources/queries/webservice/requests/";
+    private static final String REQ_PREFIX = "Test-EPCIS10-SE";
+    private static final String REQ_SUFFIX = "-Request.xml";
+    private static final String RESP_PATH = "src/test/resources/queries/webservice/responses/";
+    private static final String RESP_PREFIX = "Test-EPCIS10-SE";
+    private static final String RESP_SUFFIX = "-Response.xml";
+
+    private QueryControlClient client = new QueryControlClient();
+
+    /**
+     * Test SE1.
+     * 
+     * @throws IOException
+     *             IF an I/O error occured.
+     * @throws ServiceException
+     *             If an error in the service occured.
+     */
+    public void testSE1() throws IOException, ServiceException {
+        int testNr = 1;
+        String query = REQ_PATH + REQ_PREFIX + testNr + REQ_SUFFIX;
+        InputStream fis = new FileInputStream(query);
+        QueryResults actResults = client.poll(fis);
+        fis.close();
+
+        String resp = RESP_PATH + RESP_PREFIX + testNr + RESP_SUFFIX;
+        fis = new FileInputStream(resp);
+        QueryResults expResults = QueryResultsParser.parseQueryResults(fis);
+        fis.close();
+        QueryResultsParser.compareResults(expResults, actResults);
+    }
+
+    /**
+     * Test SE2.
+     * 
+     * @throws IOException
+     *             IF an I/O error occured.
+     * @throws ServiceException
+     *             If an error in the service occured.
+     */
+    public void testSE2() throws IOException, ServiceException {
+        fail("Authentication not supported!");
+    }
+
+    /**
+     * Test SE3.
+     * 
+     * @throws IOException
+     *             IF an I/O error occured.
+     * @throws ServiceException
+     *             If an error in the service occured.
+     */
+    public void testSE3() throws IOException, ServiceException {
+        int testNr = 3;
+        String query = REQ_PATH + REQ_PREFIX + testNr + REQ_SUFFIX;
+        InputStream fis = new FileInputStream(query);
+        QueryResults actResults = client.poll(fis);
+        fis.close();
+
+        String resp = RESP_PATH + RESP_PREFIX + testNr + RESP_SUFFIX;
+        fis = new FileInputStream(resp);
+        QueryResults expResults = QueryResultsParser.parseQueryResults(fis);
+        fis.close();
+        QueryResultsParser.compareResults(expResults, actResults);
+    }
+}
---
+++ epcis/tags/epcis-0.2.3/epcis-interop-test/src/test/java/org/fosstrak/epcis/repository/test/CallbackUnsubscribeTest.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.accada.epcis.repository.test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.rpc.ServiceException;
+
+import junit.framework.TestCase;
+
+import org.accada.epcis.queryclient.QueryControlClient;
+import org.accada.epcis.soapapi.NoSuchSubscriptionException;
+import org.accada.epcis.utils.QueryCallbackListener;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * Test for unsubscribing queries (SE44).
+ * 
+ * @author Marco Steybe
+ */
+public class CallbackUnsubscribeTest extends TestCase {
+
+    private static final String PATH = "src/test/resources/queries/webservice/requests/";
+
+    private QueryControlClient client = new QueryControlClient();
+
+    /**
+     * Tests if we receive a notification for a subscribed query, and we receive
+     * no further notification after the query is unsubscribed.
+     * 
+     * @throws IOException
+     *             If an I/O excpetion occured.
+     * @throws ServiceException
+     *             If the EPCIS query service encountered a problem.
+     * @throws ParserConfigurationException
+     *             If the parser for parsing the response could not be
+     *             configured.
+     * @throws SAXException
+     *             If the response could not be parsed.
+     */
+    public void testSE44() throws IOException, ServiceException,
+            ParserConfigurationException, SAXException {
+        final String query = "Test-EPCIS10-SE44-Request-1-Subscribe.xml";
+
+        // subscribe a query
+        InputStream fis = new FileInputStream(PATH + query);
+        client.subscribe(fis);
+        fis.close();
+
+        // start subscription response listener
+        QueryCallbackListener listener = QueryCallbackListener.getInstance();
+        if (!listener.isRunning()) {
+            listener.start();
+        }
+        System.out.println("waiting ...");
+        synchronized (listener) {
+            try {
+                listener.wait(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String resp1 = listener.fetchResponse();
+        assertNotNull(resp1);
+
+        // parse response to make sure we got back a result
+        Document epcis = parseResponse(resp1);
+        Node eventList = epcis.getElementsByTagName("EventList").item(0);
+        assertTrue(eventList.hasChildNodes());
+
+        // unsubscribe the query and wait for any response
+        client.unsubscribe("QuerySE44");
+        System.out.println("waiting ...");
+        synchronized (listener) {
+            try {
+                listener.wait(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        String resp2 = listener.fetchResponse();
+        assertNull("No response expected, but received: " + resp2, resp2);
+        listener.stopRunning();
+    }
+
+    /**
+     * Parses a string into an XML Document.
+     * 
+     * @param resp
+     *            The string to be parsed.
+     * @return The parsed XML Document.
+     * @throws ParserConfigurationException
+     *             If the parser could not be configured.
+     * @throws SAXException
+     *             If a parse error occured.
+     * @throws IOException
+     *             If an I/O error occured.
+     */
+    private Document parseResponse(final String resp)
+            throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource xmlInput = new InputSource(new StringReader(resp));
+        return builder.parse(xmlInput);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        try {
+            client.unsubscribe("QuerySE44");
+        } catch (NoSuchSubscriptionException e) {
+        }
+    }
+}
