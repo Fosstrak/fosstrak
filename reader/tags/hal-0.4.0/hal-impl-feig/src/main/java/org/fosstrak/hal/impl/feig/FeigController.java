5a44175a2e011215cbb11a799f7cb1e266adf611
roduner <roduner@b63d296a-7662-4df4-3098-00f9c2c9103a>
2008-03-21 12:23:19 +0000

    Removed tag for HAL release that ended up in the wrong module due to an error in the HAL's pom.xml.

--- reader/tags/hal-0.4.0/hal-impl-feig/src/main/java/org/fosstrak/hal/impl/feig/FeigController.java
+++
@@ -1,9 +0,0 @@
-package org.accada.hal.impl.feig;
-
-import org.accada.hal.HardwareAbstraction;
-import org.accada.hal.HardwareException;
-
-public interface FeigController extends HardwareAbstraction {
-
-	public boolean selectChannel(int address, int channel) throws HardwareException;
-}
