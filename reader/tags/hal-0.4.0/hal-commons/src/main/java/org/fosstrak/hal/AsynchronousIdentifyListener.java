76d938ba7a5c1af587e98ad7405937485f80a56e
roduner <roduner@b63d296a-7662-4df4-3098-00f9c2c9103a>
2008-03-21 12:20:18 +0000

    [maven-release-plugin]  copy for tag hal-0.4.0

---
+++ reader/tags/hal-0.4.0/hal-commons/src/main/java/org/fosstrak/hal/AsynchronousIdentifyListener.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.accada.hal;
+
+/**
+ * This interface should be implemented in order to be able to receive tag
+ * identification observations asynchronously.
+ * 
+ * @author Matthias Lampe, lampe@acm.org
+ * @author Christian Floerkemeier, floerkem@mit.edu
+ */
+public interface AsynchronousIdentifyListener {
+	
+	/**
+	 * This callback method is called whenever an asynchronous identify has been performed.
+	 * 
+	 * @param observation Contains the information about the tag identification.
+	 */
+	void asynchronousIdentifyPerformed(Observation observation);
+
+}
