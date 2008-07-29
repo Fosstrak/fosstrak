98835120b772e4ae0fc8f62a8c4182f52bfd6198
roduner <roduner@b63d296a-7662-4df4-3098-00f9c2c9103a>
2008-07-29 14:35:43 +0000

    Deleted tag (will run "mvn release:prepare" again).

--- hal/tags/hal-0.5.0/hal-impl-feig/src/main/java/org/fosstrak/hal/impl/feig/FeigController.java
+++
@@ -1,9 +0,0 @@
-package org.fosstrak.hal.impl.feig;
-
-import org.fosstrak.hal.HardwareAbstraction;
-import org.fosstrak.hal.HardwareException;
-
-public interface FeigController extends HardwareAbstraction {
-
-	public boolean selectChannel(int address, int channel) throws HardwareException;
-}
