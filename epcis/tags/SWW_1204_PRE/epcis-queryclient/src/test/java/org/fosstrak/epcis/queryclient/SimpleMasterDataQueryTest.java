c64c3ec321ede79730c2db6145f6c8b9d1ef4117
wellington <wellington@b63d296a-7662-4df4-3098-00f9c2c9103a>
2008-12-04 21:05:31 +0000

    Snapshot prior to capture client auth check-in

---
+++ epcis/tags/SWW_1204_PRE/epcis-queryclient/src/test/java/org/fosstrak/epcis/queryclient/SimpleMasterDataQueryTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Fosstrak (www.fosstrak.org).
+ *
+ * Fosstrak is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Fosstrak is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Fosstrak; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.fosstrak.epcis.queryclient;
+
+import java.net.URL;
+
+import org.fosstrak.epcis.model.QueryResults;
+import org.fosstrak.epcis.utils.QueryResultsParser;
+
+/**
+ * A simple test utility class for demonstrating how to send masterdata queries
+ * to the Fosstrak EPCIS query module.
+ * 
+ * @author Marco Steybe
+ */
+public class SimpleMasterDataQueryTest {
+
+    // Note: keep the methods in this class static in order to prevent them from
+    // being executed when building the project with Maven.
+
+    public static void main(String[] args) throws Exception {
+        // configure the query service
+        String queryUrl = "http://demo.fosstrak.org/epcis/query";
+        QueryControlClient client = new QueryControlClient();
+        client.configureService(new URL(queryUrl), null);
+
+        // create a query in its XML form and send it to the repository
+        System.out.println("Sending query:");
+        String xmlQuery = createPollXml();
+        System.out.println(xmlQuery);
+        QueryResults results = client.poll(xmlQuery);
+        // print the results to System.out
+        QueryResultsParser.queryResultsToXml(results, System.out);
+    }
+
+    /**
+     * Creates and returns a simple EPCIS masterdata query in its XML form.
+     */
+    public static String createPollXml() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<epcisq:Poll xmlns:epcisq=\"urn:epcglobal:epcis-query:xsd:1\">\n");
+        sb.append("<queryName>SimpleMasterDataQuery</queryName>\n");
+        sb.append("<params>\n");
+        sb.append("  <param>\n");
+        sb.append("    <name>includeAttributes</name>\n");
+        sb.append("    <value>true</value>\n");
+        sb.append("  </param>\n");
+        sb.append("  <param>\n");
+        sb.append("    <name>includeChildren</name>\n");
+        sb.append("    <value>true</value>\n");
+        sb.append("  </param>\n");
+        sb.append("  <param>\n");
+        sb.append("    <name>EQ_name</name>\n");
+        sb.append("    <value><string>urn:epcglobal:fmcg:loc:0614141073467</string></value>\n");
+        sb.append("  </param>\n");
+        sb.append("</params>\n");
+        sb.append("</epcisq:Poll>");
+        return sb.toString();
+    }
+}
---
+++ epcis/tags/SWW_1204_PRE/epcis-repository/src/main/java/org/fosstrak/epcis/repository/query/QueryInitServlet.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Fosstrak (www.fosstrak.org).
+ *
+ * Fosstrak is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Fosstrak is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Fosstrak; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.fosstrak.epcis.repository.query;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.sql.DataSource;
+import javax.xml.ws.Endpoint;
+
+import org.fosstrak.epcis.soap.EPCISServicePortType;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+
+/**
+ * This HttpServlet is used to initialize the QueryOperationsModule. It will
+ * read the application's properties file from the class path, read the data
+ * source from a JNDI name, and load the CXF Web service bus in order to set up
+ * the QueryOperationsModule with the required values.
+ * <p>
+ * Note: this servlet is only required if you do not wire the application with
+ * Spring! To use this servlet, and bypass Spring, replace
+ * <code>WEB-INF/web.xml</code> with <code>WEB-INF/non-spring-web.xml</code>.
+ * 
+ * @author Marco Steybe
+ */
+public class QueryInitServlet extends CXFNonSpringServlet {
+
+    private static final long serialVersionUID = -5839101192038037389L;
+
+    private static final String APP_CONFIG_LOCATION = "appConfigLocation";
+    private static final String PROP_MAX_QUERY_ROWS = "maxQueryResultRows";
+    private static final String PROP_MAX_QUERY_TIME = "maxQueryExecutionTime";
+    private static final String PROP_TRIGGER_CHECK_SEC = "trigger.condition.check.sec";
+    private static final String PROP_TRIGGER_CHECK_MIN = "trigger.condition.check.min";
+    private static final String PROP_SERVICE_VERSION = "service.version";
+    private static final String PROP_JNDI_DATASOURCE_NAME = "jndi.datasource.name";
+
+    private static final Log LOG = LogFactory.getLog(QueryInitServlet.class);
+
+    private Properties properties;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.apache.cxf.transport.servlet.CXFNonSpringServlet#loadBus(javax.servlet.ServletConfig)
+     */
+    public void loadBus(ServletConfig servletConfig) throws ServletException {
+        super.loadBus(servletConfig);
+        BusFactory.setDefaultBus(getBus());
+        if (LOG.isDebugEnabled()) {
+            getBus().getInInterceptors().add(new LoggingInInterceptor());
+            getBus().getOutInterceptors().add(new LoggingOutInterceptor());
+            getBus().getOutFaultInterceptors().add(new LoggingOutInterceptor());
+            getBus().getInFaultInterceptors().add(new LoggingInInterceptor());
+        }
+        EPCISServicePortType service = setupQueryOperationsModule(servletConfig);
+
+        LOG.debug("Publishing query operations module service at /query");
+        Endpoint.publish("/query", service);
+    }
+
+    private EPCISServicePortType setupQueryOperationsModule(ServletConfig servletConfig) {
+        loadApplicationProperties(servletConfig);
+        String jndiName = properties.getProperty(PROP_JNDI_DATASOURCE_NAME);
+        DataSource dataSource = loadDataSource(jndiName);
+
+        LOG.debug("Initializing query operations module");
+        QueryOperationsModule module = new QueryOperationsModule();
+        module.setMaxQueryRows(Integer.parseInt(properties.getProperty(PROP_MAX_QUERY_ROWS)));
+        module.setMaxQueryTime(Integer.parseInt(properties.getProperty(PROP_MAX_QUERY_TIME)));
+        module.setTriggerConditionMinutes(properties.getProperty(PROP_TRIGGER_CHECK_MIN));
+        module.setTriggerConditionSeconds(properties.getProperty(PROP_TRIGGER_CHECK_SEC));
+        module.setServiceVersion(properties.getProperty(PROP_SERVICE_VERSION));
+        module.setDataSource(dataSource);
+        module.setServletContext(servletConfig.getServletContext());
+        module.setBackend(new QueryOperationsBackendSQL());
+
+        LOG.debug("Initializing query operations web service");
+        QueryOperationsWebService service = new QueryOperationsWebService(module);
+        return service;
+    }
+
+    /**
+     * Loads the application property file and populates a java.util.Properties
+     * instance.
+     * 
+     * @param servletConfig
+     *            The ServletConfig used to locate the application property
+     *            file.
+     */
+    private void loadApplicationProperties(ServletConfig servletConfig) {
+        properties = new Properties();
+
+        // read application.properties from classpath
+        String path = "/";
+        String appConfigFile = "application.properties";
+        InputStream is = QueryInitServlet.class.getResourceAsStream(path + appConfigFile);
+
+        try {
+            if (is == null) {
+                // read properties from file specified in servlet context
+                ServletContext ctx = servletConfig.getServletContext();
+                path = ctx.getRealPath("/");
+                appConfigFile = ctx.getInitParameter(APP_CONFIG_LOCATION);
+                is = new FileInputStream(path + appConfigFile);
+            }
+            properties.load(is);
+            is.close();
+            LOG.info("Loaded application properties from " + path + appConfigFile);
+        } catch (IOException e) {
+            LOG.error("Unable to load application properties from " + path + appConfigFile, e);
+        }
+    }
+
+    /**
+     * Loads the data source from the application context via JNDI.
+     * 
+     * @param jndiName
+     *            The name of the JNDI data source holding the connection to the
+     *            database.
+     * @return The application DataSource instance.
+     */
+    private DataSource loadDataSource(String jndiName) {
+        DataSource dataSource = null;
+        try {
+            Context ctx = new InitialContext();
+            dataSource = (DataSource) ctx.lookup(jndiName);
+            LOG.info("Loaded data source via JNDI from " + jndiName);
+        } catch (NamingException e) {
+            LOG.error("Unable to load data source via JNDI from " + jndiName, e);
+        }
+        return dataSource;
+    }
+}
